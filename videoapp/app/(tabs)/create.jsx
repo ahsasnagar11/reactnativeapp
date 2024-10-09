a1ad4e1be9633b47d369abb7c3772e5804c13f96
AhsasNagar11 <ahsasyahoo11@gmail.com>
2024-10-09 20:15:12 +0530

    initial commit

---
+++ videoapp/app/(tabs)/create.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { router } from "expo-router";
+import { ResizeMode, Video } from "expo-av";
+import * as DocumentPicker from "expo-document-picker";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  View,
+  Text,
+  Alert,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+
+import { icons } from "../../constants";
+import { createVideoPost } from "../../lib/appwrite";
+import { CustomButton, FormField } from "../../components";
+import { useGlobalContext } from "../../context/GlobalProvider";
+
+const Create = () => {
+}
+
+export default Create;
---
+++ videoapp/app/(tabs)/profile.jsx
@@ -0,0 +1,15 @@
+import { router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Image, FlatList, TouchableOpacity } from "react-native";
+
+import { icons } from "../../constants";
+import useAppwrite from "../../lib/useAppwrite";
+import { getUserPosts, signOut } from "../../lib/appwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
+import { EmptyState, InfoBox, VideoCard } from "../../components";
+
+const Profile = () => {
+ 
+};
+
+export default Profile;
